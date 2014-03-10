ec9890e9669ee53d2688335d0f7d20fc62f8b248
s1247380 <m.a.asenov@gmail.com>
2014-03-10 00:36:39 +0000

    EUSA app version one

---
+++ mysite/polls/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.utils import timezone
+import datetime
+
+class Opinion(models.Model):
+	name = models.CharField(max_length=200)
+	receiver = models.CharField(max_length=200)
+	department = models.CharField(max_length=200)
+	why_thank = models.CharField(max_length=200)
+	anonymous = models.BooleanField(default=False)
+	approved = models.BooleanField(default=False)
+	pub_date = models.DateTimeField('date published')
+	def __unicode__(self):  # Python 3: def __str__(self):
+		return self.why_thank
+	def was_published_recently(self):
+    		return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+    	was_published_recently.admin_order_field = 'pub_date'
+    	was_published_recently.boolean = True
+    	was_published_recently.short_description = 'Published recently?'
+# Create your models here.
---
+++ mysite/polls/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from django.views import generic
+
+from polls.models import Opinion
+
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_poll_list'
+
+    def get_queryset(self):
+        """Return the last five published polls."""
+        return Opinion.objects.filter(approved=True)
